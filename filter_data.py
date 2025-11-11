6c9a25f45d7ae03fba6f95de23afbb97080771b3
duncanfun <u101530009@gap.kmu.edu.tw>
2025-11-11 21:23:50 +0800

    修改： 1. 修改部分語法，使用def寫法
    未完成：
    1. 複方查詢方法有問題，需要在解決

--- filter_data.py
+++ filter_data.py
@@ -27,25 +27,24 @@ need_medication2=need_medication2[need_medication2['許可證字號']!=' ']
 
 need_medication=pd.concat([need_medication[~need_medication['許可證字號'].isnull()],need_medication2])
 
-#簡化條件，先做單方
-need_medication=need_medication[need_medication['單複方']=='單方']
+def merge_contain_dose_from_tfda(need_medication):
+    sql_tfda_to_content="""
+    SELECT 成分名稱
+    , 成分代碼
+    , 含量
+    , 含量單位
+    , 許可證字號
+    FROM 詳細處方成分"""
 
-sql_tfda_to_content="""
-SELECT 成分名稱
-, 成分代碼
-, 含量
-, 含量單位
-, 許可證字號
-FROM 詳細處方成分"""
+    tfda_to_content=pd.read_sql(sql_tfda_to_content, conn)
 
-tfda_to_content=pd.read_sql(sql_tfda_to_content, conn)
+    #串TFDA的藥品含量與單位
+    need_medication=pd.merge(need_medication,tfda_to_content,how='left',on='許可證字號')
+    # FDA的成分表裡面會有重複的資料
+    need_medication=need_medication.drop_duplicates(subset=['許可證字號','成分名稱','含量', '含量單位'])
+    need_medication=need_medication.dropna(subset='成分名稱')
+    return need_medication
 
-
-
-need_medication=pd.merge(need_medication,tfda_to_content,how='left',on='許可證字號')
-# FDA的成分表裡面會有重複的資料
-need_medication=need_medication.drop_duplicates(subset=['許可證字號','成分名稱','含量', '含量單位'])
-need_medication=need_medication.dropna(subset='成分名稱')
 
 def find_rxcui(tty_type, keyword):
     #rxnom sqlite link
@@ -75,6 +74,8 @@ def add_rxcui_in_pin_scdc(row, tty_type):
             return row
     elif (tty_type=='IN') or (tty_type=='PIN'):
         keyword=row['成分名稱']
+    elif tty_type=='MIN':
+        keyword=row['成分串接']
     #tty_type='PIN'
     in_df=find_rxcui(tty_type, keyword)
     if in_df.empty==False:
@@ -95,25 +96,51 @@ def get_text_using_node_from_url(url: str, node: str):
         return None
 
 def find_without_rxnorm_db(row):
+    #沒有資料才做
     if (row['IN']==None) & (row['PIN']==None) & (row['SCDC']==None):
         keyword=row['成分名稱']
-        print(keyword)
+        #依規則文字處理
         keyword=keyword.replace(' ', '+')
         #解析XML部分代處理
         get_rxcui=f'https://rxnav.nlm.nih.gov/REST/rxcui?name={keyword}&search=1'
         rxcui=get_text_using_node_from_url(get_rxcui,'.//rxnormId')
         if rxcui is not None:
+            #有找到rxcui才做下一步
             get_tty=f'https://rxnav.nlm.nih.gov/REST/rxcui/{rxcui}/properties'
             tty=get_text_using_node_from_url(get_tty,'.//tty')
-            print(tty)
-            print(rxcui)
             row[tty]=rxcui
     return row
 
-need_medication=need_medication.apply(add_rxcui_in_pin_scdc,args=('IN',),axis=1)
-need_medication=need_medication.apply(add_rxcui_in_pin_scdc,args=('PIN',),axis=1)
-need_medication=need_medication.apply(add_rxcui_in_pin_scdc,args=('SCDC',),axis=1)
+def for_single_contain(need_medication):
+    """單方可以先串上FDA的含量資料後，可以依序查詢
+    1. 單方的IN、PIN、SCDC
+    2. 由於rxnorm_prescribe裡面只有FDA目前有處方的藥品，有已經沒有處方藥品的DB好像要會員才能取得，因此用API去取得目前不再FDA處方中的
+    
+    剩餘藥物後續再另外當個案處理
+    """
+    need_medication=need_medication[need_medication['單複方']=='單方']
+    #串上FDA的成分與含量資料
+    need_medication=merge_contain_dose_from_tfda(need_medication)
 
-need_medication=need_medication.head(1).apply(find_without_rxnorm_db, axis=1)
+    
+    need_medication=need_medication.apply(add_rxcui_in_pin_scdc,args=('IN',),axis=1)
+    need_medication=need_medication.apply(add_rxcui_in_pin_scdc,args=('PIN',),axis=1)
+    need_medication=need_medication.apply(add_rxcui_in_pin_scdc,args=('SCDC',),axis=1)
 
+    need_medication=need_medication.apply(find_without_rxnorm_db, axis=1)
+    return need_medication
+    #need_medication=pd.read_pickle('files/temp_single_contain.pkl')
 
+#複方應該要用許可證字號做group去分每個group操作
+need_medication=need_medication[need_medication['單複方']=='複方']
+need_medication=merge_contain_dose_from_tfda(need_medication)
+need_medication_grouped = (need_medication.groupby("許可證字號")["成分名稱"]
+             .agg(lambda x: " / ".join(sorted(pd.Series(x).unique(), key=str.lower)))
+             #              分隔字符       不區分大小寫排序   排除重複
+             .reset_index()
+             .rename(columns={"成分名稱": "成分串接"}))
+need_medication_grouped=need_medication_grouped.apply(add_rxcui_in_pin_scdc,args=('MIN',),axis=1)
+#先筆記一下，這個方法找出來的MIN只有8個，主要應該是因為MIN的的成分通常沒有鹽基，但TFDA的資料幾乎都有鹽基
+#可能的解決方法
+#1. 用單方查好的IN去找MIN
+#2. 用split把鹽基去掉
